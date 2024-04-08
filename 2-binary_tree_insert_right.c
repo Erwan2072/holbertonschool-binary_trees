2e8fc47136f1ca661d0958559e28980d21fb828e
antonin-crypto <akamis53@gmail.com>
2024-04-08 12:58:16 +0000

    modified code 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,27 +10,25 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-	{
+	binary_tree_t *temp = NULL, *node = NULL;
+
+	if (!parent)
 		return (NULL);
-	}
-	binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
+
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	if (parent->right)
 	{
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-
-		new_node->n = value;
-		new_node->left = NULL;
-		new_node->right = NULL;
-
-		if (parent->right != NULL)
-		{
-			new_node->right = parent->right;
-		}
-
-		parent->right = new_node;
+		temp = parent->right;
+		node->right = temp;
+		temp->parent = node;
 	}
-	return (new_node);
+	parent->right = node;
+	return (node);
 }
