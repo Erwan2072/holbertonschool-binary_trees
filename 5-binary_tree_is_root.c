1fcf27839915e551c1764411c2d47b007118b409
antonin-crypto <akamis53@gmail.com>
2024-04-09 06:56:20 +0000

    bettystyle

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_is_root - checks if a given node is a root
  * @node: pointer to the node to check
@@ -9,7 +8,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 	{
-        return 0;
+		return (0);
 	}
 	if (node->parent == NULL)
 	{
